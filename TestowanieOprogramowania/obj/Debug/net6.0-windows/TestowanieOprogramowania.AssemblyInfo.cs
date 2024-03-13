b11d47d515625af77dfc298d312552e2eafda0b9
hoodie <karolpytlos25@gmail.com>
2024-03-13 22:46:05 +0100

    dsadsa

--- TestowanieOprogramowania/obj/Debug/net6.0-windows/TestowanieOprogramowania.AssemblyInfo.cs
+++ TestowanieOprogramowania/obj/Debug/net6.0-windows/TestowanieOprogramowania.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestowanieOprogramowania")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81c7956f058ca72613a52e22e243b1dffc513a9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestowanieOprogramowania")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestowanieOprogramowania")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
